6347c775931b45a8d438a44aa8eb98709e9bf7b0
premdandin08-oss <premdandin08@gmail.com>
2025-10-18 09:41:31 +0530

    Saving the modified file

--- cie.py
+++ cie.py
@@ -1,4 +1,6 @@
 import math
 radius = float(input("Enter the radius of the circle: "))
 area = 3.142 * (radius ** 2)
+circumference = 2 * 3.142 * radius
 print(f"The area of the circle is:", area)
+print(f"The circumference of the circle is:", circumference)
